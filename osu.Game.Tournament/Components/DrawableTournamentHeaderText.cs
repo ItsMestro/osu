048866fc2cb2648b68fa3c3e7f02ac106f723ffc
Marcus "Mestro" Nordgren <itsmestro@gmail.com>
2020-03-16 15:08:06 +0100

    4DMWC2 Text

--- osu.Game.Tournament/Components/DrawableTournamentHeaderText.cs
+++ osu.Game.Tournament/Components/DrawableTournamentHeaderText.cs
@@ -1,37 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Textures;
+using osu.Game.Graphics;
 
 namespace osu.Game.Tournament.Components
 {
-    public class DrawableTournamentHeaderText : CompositeDrawable
+    public class DrawableTournamentHeaderText : TournamentSpriteText
     {
         public DrawableTournamentHeaderText()
         {
-            InternalChild = new TextSprite();
-
-            Height = 22;
-            RelativeSizeAxes = Axes.X;
-        }
-
-        private class TextSprite : Sprite
-        {
-            [BackgroundDependencyLoader]
-            private void load(TextureStore textures)
-            {
-                RelativeSizeAxes = Axes.Both;
-                FillMode = FillMode.Fit;
-
-                Anchor = Anchor.Centre;
-                Origin = Anchor.Centre;
-
-                Texture = textures.Get("header-text");
-            }
+        Text = "4 Digit Mania World Cup 2";
+        Font = OsuFont.Torus.With(size: 26, weight: FontWeight.SemiBold);
         }
     }
 }
